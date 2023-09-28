50a692f993b3a7f301b52a145e9e24b38245abec
Nikolai Sviridov <nikolai.sviridov@jetbrains.com>
2023-09-28 18:04:16 +0200

    feat: remove unused set last commit timestamp

--- src/main/kotlin/org/jetbrains/research/ictl/riskypatterns/calculation/processors/CommitProcessor.kt
+++ src/main/kotlin/org/jetbrains/research/ictl/riskypatterns/calculation/processors/CommitProcessor.kt
@@ -35,10 +35,6 @@ class CommitProcessor(private val context: BusFactorComputationContext) {
     }
   }
 
-  fun setLastCommit(lastCommitTimestamp: Long) {
-    context.lastCommitCommitterTimestamp = lastCommitTimestamp
-  }
-
   private fun getReviewers(message: String): List<String> {
     val idx = message.indexOf(REVIEW_START_TOKEN)
     if (idx == -1) return emptyList()
