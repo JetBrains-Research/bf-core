38cdc407d16a7cbb8e2c70769ab20e386895024a
Nikolai Sviridov <nikolai.sviridov@jetbrains.com>
2023-09-29 14:18:26 +0200

    refactor: move class

--- src/main/kotlin/org/jetbrains/research/ictl/riskypatterns/calculation/BusFactorComputationContext.kt
+++ src/main/kotlin/org/jetbrains/research/ictl/riskypatterns/calculation/BusFactorComputationContext.kt
@@ -8,24 +8,6 @@ import org.jetbrains.research.ictl.riskypatterns.calculation.mappers.FileMapper
 import org.jetbrains.research.ictl.riskypatterns.calculation.mappers.UserMapper
 
 @Serializable
-data class BusFactorConfigSnapshot(
-  val useReviewers: Boolean,
-  val weightedAuthorship: Boolean,
-  val ignoreExtensions: Set<String>,
-) {
-  companion object {
-    private const val DEFAULT_USE_REVIEWERS = false
-    private const val DEFAULT_WEIGHTED_AUTHORSHIP = true
-
-    fun getDefault() = BusFactorConfigSnapshot(
-      DEFAULT_USE_REVIEWERS,
-      DEFAULT_WEIGHTED_AUTHORSHIP,
-      setOf(),
-    )
-  }
-}
-
-@Serializable
 data class BusFactorComputationContext(
   override val userMapper: UserMapper = UserMapper(),
   override val fileMapper: FileMapper = FileMapper(),
